9093ac60fc9fccd170deac56b1b4a612edfb1e18
ravi4all <ravikanttyagi.2295@gmail.com>
2017-05-22 11:44:03 +0530

    Add files via upload

--- 02-IfElse/StonePaperScissor.py
+++ 02-IfElse/StonePaperScissor.py
@@ -4,28 +4,33 @@ cpu_choice = ['stone','paper','scissor']
 
 cpu = random.choice(cpu_choice)
 
-user_input = input("Enter your choice : ")
+gameExit = True
 
-if user_input == cpu:
-    print("CPU Choice is : ",cpu)
-    print('Tie')
-elif user_input == 'stone' and cpu == 'scissor':
-    print("CPU Choice is : ",cpu)
-    print('You Win')
-elif user_input == 'paper' and cpu == 'stone':
-    print("CPU Choice is : ",cpu)
-    print('You Win')
-elif user_input == 'scissor' and cpu == 'paper':
-    print("CPU Choice is : ",cpu)
-    print('You Win')
-elif user_input == 'stone' and cpu == 'paper':
-    print("CPU Choice is : ",cpu)
-    print('CPU Win')
-elif user_input == 'paper' and cpu == 'scissor':
-    print("CPU Choice is : ",cpu)
-    print('CPU Win')
-elif user_input == 'scissor' and cpu == 'stone':
-    print("CPU Choice is : ",cpu)
-    print('CPU Win')
-else:
-    print("Wrong choice,Enter again")
+while gameExit:
+
+    user_input = input("Enter your choice : ")
+
+    if user_input == cpu:
+        print("CPU Choice is : ",cpu)
+        print('Tie')
+    elif user_input == 'stone' and cpu == 'scissor':
+        print("CPU Choice is : ",cpu)
+        print('You Win')
+    elif user_input == 'paper' and cpu == 'stone':
+        print("CPU Choice is : ",cpu)
+        print('You Win')
+    elif user_input == 'scissor' and cpu == 'paper':
+        print("CPU Choice is : ",cpu)
+        print('You Win')
+    elif user_input == 'stone' and cpu == 'paper':
+        print("CPU Choice is : ",cpu)
+        print('CPU Win')
+    elif user_input == 'paper' and cpu == 'scissor':
+        print("CPU Choice is : ",cpu)
+        print('CPU Win')
+    elif user_input == 'scissor' and cpu == 'stone':
+        print("CPU Choice is : ",cpu)
+        print('CPU Win')
+    else:
+        print("Wrong choice,Enter again")
+        gameExit = False
---
+++ 04-Sets/SetsAndSetOperations.py
@@ -0,0 +1,11 @@
+A = {1,2,3}
+B = {3,4,5,6}
+print(A | B)
+
+print(A & B)
+
+print(A - B)
+
+print(B - A)
+
+print(A ^ B)
